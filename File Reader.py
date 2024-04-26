27db6667b2de276b0c0967d256f901be860d54ae
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-04-25 21:03:16 -0800

    updated program

--- File Reader.py
+++ File Reader.py
@@ -1,5 +1,23 @@
+path = input("Input a text file path: ")
+
+purpose = input("How can i help you? (Read/Write/Append/Clear): ")
+
 try:
-    with open("C:\\Users\\Admin\\Desktop\\test.txt.txt") as file:
-        print(file.read())
+    if purpose.lower() == "read":
+        with open (path, 'r') as file:
+            print(file.read())
+    elif purpose.lower() == "write":
+        with open(path, 'w') as file:
+            input = input("Input a text: ")
+            file.write(input)
+    elif purpose.lower() == "append":
+        with open(path, 'a') as file:
+            input = input("Input a text: ")
+            file.write(input)
+    elif purpose.lower() == "clear":
+        with open(path, 'r+')as file:
+            file.seek(0)
+            file.truncate()
 except FileNotFoundError:
-    print("Di nahanap ang file haha")
+    print("such file does not exist")
+
