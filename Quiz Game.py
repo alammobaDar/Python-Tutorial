aedb4f8502817cf26efc81e4820b983e2670fded
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-04-27 22:50:23 -0800

    Updated quiz game

--- Quiz Game.py
+++ Quiz Game.py
@@ -1,13 +1,3 @@
-def new_game ():
-    pass
-def check_answer():
-    pass
-def score_display():
-    pass
-def try_again():
-    pass
-#---------------------------
-
 questions = {"What is the chemical symbol for Gold?" : "D",
              "In what year was the first iPhone released?": "B",
              "What is the tallest mountain in the world?": "B",
@@ -18,15 +8,55 @@ choices = ["A.Gd B.Go\nC.Ag D.Au",
            "A. K2 B. Mount Everest\nC. Mount Kilimanjaro D. Denali",
            "A. Leonardo da Vinci B. Michelangelo\nC. Raphael D. Caravaggio"]
 
-print("Welcome to our Quiz Game!\n")
+#_____________________________________________________________
+def new_game ():
+    print("Welcome to our Quiz Game!\n")
 
-for i, question in enumerate(questions.keys()):
-    print(question)
-    for choice in choices[i]:
-        print(choice, end = "")
-    answer = input("\nYour Answer?: ")
+    score = 0
 
+    for i, question in enumerate(questions.keys()):
+        print("*-----------------------------------------*")
+        print(question)
+        for choice in choices[i]:
+            print(choice, end = "")
+        answer = input("\nYour Answer?: ").upper()
+        score = check_answer(score, answer, question)
 
+    score_display(score)
+    try_again()
+#_____________________________________________________________
+def check_answer(score, answer, question):
+
+    correct_answers = questions[question]
+    if answer == correct_answers:
+        score += 10
+    return score
+#_____________________________________________________________
+def score_display(score):
+
+   print("\nScore: ", score)
+
+   if score == 40:
+       print("You are Excellent")
+   elif score == 30:
+       print("That is great!")
+   elif score == 20:
+       print("You are good!")
+   elif score == 10:
+       print("Fair Enough!")
+   else:
+       print("Study more!")
+#_____________________________________________________________
+def try_again():
+    reset = input("\nDo you want to try again? (Y/N)").upper()
+
+    if reset == "Y":
+        new_game()
+    else:
+        print("\nThank you for your time!")
+#_____________________________________________________________
+
+new_game()
 print("\nThe end!")
 
 
