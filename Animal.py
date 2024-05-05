286fd8a01df7442b27ee6018aebfe3e54b689f5b
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-05-04 21:14:42 -0800

    OOP in python

--- Animal.py
+++ Animal.py
@@ -1,12 +1,24 @@
+
+
 class Animal:
-    def __init__(self, Name, Species, Color, Size):
-        self.Name = Name
-        self.Species = Species
-        self.Color = Color
-        self.Size = Size
+    # def __init__(self, Name, Species, Color, Size):
+    #     self.Name = Name
+    #     self.Species = Species
+    #     self.Color = Color
+    #     self.Size = Size
 
     def eat(self):
         print(self.Name + " the " + self.Species + " is eating ")
 
     def sleep(self):
         print(self.Name + " the " + self.Species + " is Sleeping ")
+class Mammal(Animal):
+    def reproduction(self):
+        print("Mammals don't lay eggs!")
+class Dog(Mammal):
+    def bark(self):
+        print("The Dog is barking!")
+
+dog = Dog()
+dog.bark()
+dog.reproduction()
