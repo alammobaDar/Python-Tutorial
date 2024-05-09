15db70ae56a5f1720fa02d1ce18fe8df54deab1d
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-05-08 23:38:00 -0800

    Converting roman into numbers

---
+++ Roman_numerals.py
@@ -0,0 +1,29 @@
+def convert(x):
+    numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    result = 0
+    prev_value = 0
+
+    for char in x[::-1]:
+        value = numerals[char]
+
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+
+            prev_value = value
+
+    return result
+
+
+# ___________________
+
+print(convert("IX"))
