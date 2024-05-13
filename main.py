2376e43e3d485615672513f605060faa9de877e6
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-05-12 22:09:41 -0800

    list comprehension

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
-# reduce() function
-from functools import reduce
-# example of applications of reduce()
+# list comprehension
+## INSTEAD OF USING THIS:
 
-# Summarizing data :Calculating the sum, product,
-# maximum, or minimum value of a collection of data.
+List = []
+for i in range(1,11):
+    List.append(i*i)
+print(List)
 
-numbers = [1, 2, 3, 4, 5, 6]
+## Use this instead:
+list = [i * i for i in range (1,11)]
+print(list)
 
-result = reduce(lambda x, y: x + y, numbers)
-print(numbers , "\nSum of Numbers: ", result)
+### Examples:
 
-# Aggregating data: Combining data from multiple sources
-# or reducing a sequence of values to a single value.
+sections = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-String = ["Edrich"," ", "Darren"," ", "Santuyo" ]
-combined_string = reduce(lambda x, y: x + y, String)
-print("\n",String , "\nCombined string: ", combined_string)
+list = [i for i in sections if i <= 6]
+print("\nThe higher sections are: ", list)
 
-## finds the largest number
-numbers1 = [54, 6, 99, 32, 17]
-largest = reduce(lambda x,y: x if x > y else y, numbers1)
-print("\n",numbers1, "\nThe largest number is: ", largest)
-
-
-
-# Iterative Operations:  Performing iterative operations
-# such as factorials, exponentiation, or any other
-# computation that requires combining elements of an iterable.
-
-def factorial(x):
-    return reduce(lambda x,y: x * y, range(1, x+1))
-print("\nThe result will be: ", factorial(5))
-
-# Functional programming: Implementing functional programming
-# concepts such as folding, reducing, or accumulating data.
-
-data = [14,82, 45,23,6]
-result = reduce(lambda x,y: x-y if x>y else y - x, data)
-print(data ,"\nThe result of the custom operation is: ", result)
-
-
+list = [i if i <= 5 else str(i)+" (Lower section)" for i in sections]
+print("\nThe higher sections are: ", list)
