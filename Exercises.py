74d06cfbc3d49e8f2e2e4c6989d1b97d031f93f8
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-05-15 23:37:06 -0800

    FizzBuzz exercises

---
+++ Exercises.py
@@ -0,0 +1,13 @@
+# FizzBuzz
+
+for i in range(0,300):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 4 == 0:
+        print("slavery")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
