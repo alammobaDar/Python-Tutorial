806337bdcb7455423e4b38691ea62b0bed70c485
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-05-17 04:59:27 -0800

    prime numbers

--- Exercises.py
+++ Exercises.py
@@ -3,35 +3,15 @@
 def is_prime(num):
     if num <= 1:
         return False
-    for i in range(2, int(num**0.5)+1):
-        print(i)
-        if num % i ==0:
+    for i in range(2, int(num ** 0.5) +1):
+        if num % i == 0:
             return False
     return True
-
-Enter a number: 53
-2
-3
-4
-5
-6
-7
-2
-3
-4
-5
-6
-7
-Its a prime
 
 num = int(input("Enter a number: "))
 is_prime(num)
 
-
-if is_prime(num)== True:
-    print("Its a prime")
+if is_prime(num):
+    print("It's a prime!")
 else:
-    print("Its, not")
-
-# for i in range(2, int(20**0.5)+1):
-#         print(i)
+    print("it's not")
