344bd7d240dba920912bc370b6a77f222da5f50e
Edrich Darren <edrichdarrensantuyo172030@gmail.com>
2024-05-17 12:12:56 -0800

    password checker

--- Exercises.py
+++ Exercises.py
@@ -1,8 +1,27 @@
-# vowel counter
+# password checker
 
-def reversed_string(string):
-    reversed = string[::-1]
-    print("The reversed string will be: ", reversed)
+import re
+def is_valid(password):
+    if len(password) < 8:
+        print("Password must have more than 8 characters")
+        return False
+    if not re.search("[a-z]", password):
+        print("Password must have an lowercase letter")
+        return False
+    if not re.search("[A-Z]", password):
+        print("Password must have an Uppercase letter")
+        return False
+    if not re.search("[0-9]", password):
+        print("Password must have a number on it")
+        return False
+    if not re.search("[!@#$%^&*()_+={}:;""''<>,.?/]", password):
+        print("Password must have a special character")
+        return False
+    return True
 
-string = input("Enter a string: ")
-reversed_string(string)
+password = input("Enter a pasword: ")
+
+if is_valid(password):
+    print("Your password is valid!")
+else:
+    print("Your password is not valid")
